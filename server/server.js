5ffc4a456512c758cd1257ddcb6b1a9cf83c9e83
Hugo Rodriguez <hi.rodriguez1998@gmail.com>
2021-03-23 17:54:38 -0300

    Added mongoDB configuration

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import devBundle from './devBundle';
-import path from 'path';
-import template from './../template';
 const app = express();
 devBundle.compile(app);
 const CURRENT_WORKING_DIR = process.cwd();
+
+import path from 'path';
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR,
     'dist')));
 
+import template from './../template';
 app.get('/', (req, res) => {
     res.status(200).send(template())
 });
@@ -20,3 +21,9 @@ app.listen(port, function onStart(err) {
     console.info('Server started on port %s.', port)
 });
 
+import { MongoClient } from 'mongodb';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup';
+MongoClient.connect(url, (err, db) => {
+    console.log("Connected successfully to mongodb server");
+    db.close()
+});
